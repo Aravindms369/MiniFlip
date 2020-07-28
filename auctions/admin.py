d30a25248d7a2135816627e8db6e48b773e4a971
Aravindms369 <55455165+Aravindms369@users.noreply.github.com>
2020-07-28 17:40:27 +0530

    final changes

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,11 +1,3 @@
 from django.contrib import admin
-from .models import User, Listing, Bid,Comment,Watchlist,Closedbid,Alllisting
 
 # Register your models here.
-admin.site.register(User)
-admin.site.register(Listing)
-admin.site.register(Bid)
-admin.site.register(Comment)
-admin.site.register(Watchlist)
-admin.site.register(Closedbid)
-admin.site.register(Alllisting)
